1b4a898f1fee241600fb01353373f92b708552cd
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-11-26 15:09:02 +0300

    generating necessary modules

---
+++ Blog-UI/src/app/Modules/authentication/authentication.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthenticationRoutingModule } from './authentication-routing.module';
+import { AuthenticationComponent } from './authentication.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { LoginComponent } from './Components/login/login.component';
+import { AccountComponent } from './Components/account/account.component';
+import { UpdateDetailsComponent } from './Components/update-details/update-details.component';
+import { ChangePasswordComponent } from './Components/change-password/change-password.component';
+import { ForgotPasswordComponent } from './Components/forgot-password/forgot-password.component';
+
+
+@NgModule({
+  declarations: [
+    AuthenticationComponent,
+    RegisterComponent,
+    LoginComponent,
+    AccountComponent,
+    UpdateDetailsComponent,
+    ChangePasswordComponent,
+    ForgotPasswordComponent
+  ],
+  imports: [
+    CommonModule,
+    AuthenticationRoutingModule
+  ]
+})
+export class AuthenticationModule { }
--- Blog-UI/src/app/app.module.ts
+++ Blog-UI/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './Components/homepage/homepage.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { AboutBlogComponent } from './Components/about-blog/about-blog.component';
+import { ContactMeComponent } from './Components/contact-me/contact-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    NavbarComponent,
+    FooterComponent,
+    AboutBlogComponent,
+    ContactMeComponent
   ],
   imports: [
     BrowserModule,
