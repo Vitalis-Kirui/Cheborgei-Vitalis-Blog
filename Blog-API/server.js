ce659a4475df236f3503cde854858ebdff1f78f5
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-11-26 23:51:38 +0300

    setting up server

---
+++ Blog-API/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const config = require('./config/config-variables');
+const { dbConnectionString } = require('./config/config-variables');
+
+// Initializing app
+const app = express();
+
+// Using cors
+app.use(cors());
+
+// Using body-parser
+app.use(bodyParser.json());
+
+mongoose.connect(config.dbConnectionString)
+    .then((success) => {
+        app.listen(3000);
+        console.log("Database connection established")
+    })
+    .catch((error) => { 
+        console.log(error);
+    });
