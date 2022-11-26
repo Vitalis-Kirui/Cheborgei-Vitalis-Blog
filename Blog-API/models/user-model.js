1f5ac40abd33cd14c21a92c18e3fcd631c045aaa
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-11-27 00:18:16 +0300

    creating user model

---
+++ Blog-API/models/user-model.js
@@ -0,0 +1,34 @@
+const monggose = require('mongoose');
+
+const Schema = monggose.Schema;
+
+const userSchema = new Schema({
+    firstname: {
+        type: String,
+        required: true,
+    },
+    lastname: {
+        type: String,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+    },
+    subscribe: {
+        type: Boolean,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    confirmPassword: {
+        type: String,
+        required: true,
+    }
+});
+
+module.exports = monggose.model('User', userSchema, 'users');
