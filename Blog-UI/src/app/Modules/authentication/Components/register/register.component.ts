b3a7353d4a91c75a4e64907aacbef3b2bb3cfd74
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-11-26 18:00:26 +0300

    getter function for form fields

--- Blog-UI/src/app/Modules/authentication/Components/register/register.component.ts
+++ Blog-UI/src/app/Modules/authentication/Components/register/register.component.ts
@@ -19,6 +19,41 @@ export class RegisterComponent implements OnInit {
     this.router.navigate(['authentications/login']);
   }
 
+  // Fields getter functions
+  // First name getter function
+  get firstname() {
+    return this.registrationForm.get('firstname');
+  }
+
+  // Last name getter function
+  get lastname() {
+    return this.registrationForm.get('lastname');
+  }
+
+  // Username getter function
+  get username() {
+    return this.registrationForm.get('username');
+  }
+
+  // Email getter function
+  get email() {
+    return this.registrationForm.get('email');
+  }
+
+  // Subscription getter
+  get subscribe() {
+    return this.registrationForm.get('subscribe');
+  }
+
+  // Password getter function
+  get password() {
+    return this.registrationForm.get('password');
+  }
+
+    //Confirm Password getter function
+  get confirmpassword() {
+    return this.registrationForm.get('confirmpassword');
+  }
   ngOnInit() {
     this.registrationForm = this.fbService.group({
 
@@ -29,7 +64,7 @@ export class RegisterComponent implements OnInit {
       subscribe:[false],
       password: ['', Validators.required],
       confirmpassword: ['', Validators.required]
-      
+
     })
 
   }
