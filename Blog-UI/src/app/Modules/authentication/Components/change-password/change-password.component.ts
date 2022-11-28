97c56c6cd300027502a3a2030544e6d148c909b1
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-11-28 22:37:01 +0300

    creating change password form model and validation

--- Blog-UI/src/app/Modules/authentication/Components/change-password/change-password.component.ts
+++ Blog-UI/src/app/Modules/authentication/Components/change-password/change-password.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { passwordMatchValidator } from 'src/app/Validators/password-match';
 
 @Component({
   selector: 'app-change-password',
@@ -7,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangePasswordComponent implements OnInit {
 
-  constructor() { }
+  // Update form declaration
+  updateForm! : FormGroup;
 
-  ngOnInit(): void {
+  constructor(private fbService: FormBuilder) { }
+  
+  // Getter functions
+  // oldpassword
+  get oldpassword() {
+    return this.updateForm.get('oldpassword');
+  }
+
+  // New password
+  get newpassword() {
+    return this.updateForm.get('password');
+  }
+
+  // Confirm new password
+  get confirmnewpassword() {
+    return this.updateForm.get('confirmpassword');
+  }
+
+  ngOnInit() {
+    this.updateForm = this.fbService.group({
+      oldpassword: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmpassword: ['', [Validators.required]]
+    },
+      {validator : passwordMatchValidator}
+    );
+  }
+
+  // Update password function
+  updatePassword() {
+
+    console.log(this.updateForm.value)
+    this.updateForm.reset();
+    
   }
 
 }
