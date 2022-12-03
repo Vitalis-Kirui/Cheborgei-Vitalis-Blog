8985011841185d5d2a9583aae6969b0179be0481
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-12-03 13:53:28 +0300

    creating blog requests routes

---
+++ Blog-API/routes/blog-request-routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const BlogRequestControllers = require('../controllers/blog-request-controllers');
+
+// Create a new blog request
+router.post('/create-request', BlogRequestControllers.createBlogRequest);
+
+// Get all blog requests
+router.get('/all-requests', BlogRequestControllers.getBlogRequests);
+
+// Get a single blog route
+router.get('/blog-request/:id', BlogRequestControllers.getSingleBlogRequest);
+
+// Delete a blog route
+router.delete('/blog-request/:id', BlogRequestControllers.deleteBlogRequests);
+
+module.exports = router;
