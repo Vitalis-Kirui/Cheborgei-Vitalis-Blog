0a2e74bf338f3dd1d13f922ae736908d57ea457a
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-12-03 13:21:40 +0300

    creating blog request model

--- Blog-API/models/blog-requests-model.js
+++ Blog-API/models/blog-requests-model.js
@@ -1,22 +1,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const blogSchema = new Schema({
-    blogimage: {
-        type: String,
-    },
+const blogRequestSchema = new Schema({
     title: {
         type: String,
+        required: true
     },
     category: {
         type: String,
+        required: true
     },
-    description: {
+    blog: {
         type: String,
         required: true
     }
-},
-    {timestamps: true}
-);
+});
 
-module.exports = mongoose.model('Blog', blogSchema, 'blogs');
+module.exports = mongoose.model('Blog-request', blogRequestSchema, 'blog-requests');
