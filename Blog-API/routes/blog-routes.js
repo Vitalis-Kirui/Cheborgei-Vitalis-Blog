49013b1ddd596b8ab483bfa83ceaa36142a1612e
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-12-05 17:36:02 +0300

    routes for dormant and active routes

--- Blog-API/routes/blog-routes.js
+++ Blog-API/routes/blog-routes.js
@@ -8,14 +8,20 @@ router.post('/create-blog', blogControllers.createBlog);
 // get all blog route
 router.get('/all-blogs', blogControllers.getBlogs);
 
+// Getting new blogs
+router.get('/new-blogs', blogControllers.newBlogs);
+
+//Getting active blog routes
+router.get('/active-blogs', blogControllers.activeBlogs);
+
+// Getting inactive blog routes
+router.get('/inactive-blogs', blogControllers.dormantBlogs);
+
 // get a single blog route
 router.get('/blog/:id', blogControllers.getSingleBlog);
 
 // Updating a single blog route
 router.put('/update-blog/:id', blogControllers.updateSingleBlog);
-
-// Getting new blogs
-router.get('/new-blogs', blogControllers.newBlogs);
 
 // Delete a single blog route
 router.delete('/blog/:id', blogControllers.deleteBlog);
