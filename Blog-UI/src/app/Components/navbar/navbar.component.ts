c1bce58b9e30f0911fb68a783a5ae81a9ccd1eca
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-12-08 16:55:13 +0300

    creating login and logout logic

--- Blog-UI/src/app/Components/navbar/navbar.component.ts
+++ Blog-UI/src/app/Components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/Services/authentication.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public service: AuthenticationService) { }
+  
+  // Logout function
+  logoutUser() {
+    this.service.logoutUser();
+  }
 
   ngOnInit(): void {
   }
