e862485fa058dd8aef2f92e71569a6d0572008c6
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-12-08 17:12:33 +0300

    consuming user profile endpoint

--- Blog-UI/src/app/Modules/authentication/Components/account/account.component.ts
+++ Blog-UI/src/app/Modules/authentication/Components/account/account.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/Services/authentication.service';
 
 @Component({
   selector: 'app-account',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountComponent implements OnInit {
 
-  constructor() { }
+  // User information
+  userDetails: any = {};
 
-  ngOnInit(): void {
+  constructor(private accountService: AuthenticationService) { }
+
+  ngOnInit() {
+    // Fetching user information
+    this.accountService.getUserProfile()
+      .subscribe((userData) => {
+        this.userDetails = userData.user;
+        console.log(userData);
+      },
+        error => {
+          console.log(error);
+        }
+      )
+
   }
 
 }
--- Blog-UI/src/app/Services/authentication.service.ts
+++ Blog-UI/src/app/Services/authentication.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -30,6 +31,11 @@ export class AuthenticationService {
     localStorage.removeItem('token');
 
     this.router.navigate(['/']);
+  };
+
+  // User profile/account
+  getUserProfile(): Observable<any> { 
+    return this.http.get(environment.userProfileUrl)
   };
 
 }
