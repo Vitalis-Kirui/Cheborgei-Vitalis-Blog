6085e7e5f9ef5621ee9756408da1c7f1ae3de13a
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-12-19 23:23:57 +0300

    consuming messages endpoints in messages service

--- Blog-UI/src/app/Services/messages.service.ts
+++ Blog-UI/src/app/Services/messages.service.ts
@@ -1,9 +1,38 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessagesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  
+  // creating a new message
+  sendMessage(message: any) {
+    return this.http.post<any>(environment.createMessageUrl, message)
+  }
+
+  // Fetching all messages
+  getAllMessages(): Observable<any> {
+    return this.http.get<any>(environment.getAllMessagesUrl);
+  }
+
+  // fetching new messages
+  getNewMessages(): Observable<any> {
+    return this.http.get<any>(environment.getTodayMessagesUrl);
+  }
+
+  // getting a single message
+  getSingleMessage(): Observable<any> {
+    return this.http.get<any>(environment.getTodayMessagesUrl);
+  }
+
+  // Deleting messages
+  deleteMessage(id: any) {
+    return this.http.delete<any>(environment.deleteMessageUrl, id);
+  }
+
 }
